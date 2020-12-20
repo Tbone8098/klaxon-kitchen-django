af0bd7aab466c70d672f07be4498bb7c054a7267
Tyler <thibault.tyler@gmail.com>
2020-12-20 01:44:43 -0800

    init commit - lol

---
+++ klaxonKitchen/kitchens/views.py
@@ -0,0 +1,66 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+import datetime
+from kitchens.models import *
+
+# Create your views here.
+def dashboard(request):
+    context = {
+        'allKitchens': Kitchen.objects.all(),
+        'allOrders': Order.objects.all(),
+    }
+    return render(request, 'dashboard.html', context)
+
+def add_kitchen(request):
+    errors = Kitchen.objects.kitchen_validator(request.POST)
+
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+    else:
+
+        kitchen = Kitchen.objects.create(
+            name=request.POST['kitchen_name'],
+            designation_id=request.POST['designationId']
+            )
+    return redirect("/")
+
+def add_order(request):    
+    # errors = Order.objects.order_validator(request.POST)
+
+    # if len(errors) > 0:
+    #     for key, value in errors.items():
+    #         messages.error(request, value)
+    # else:
+    kitchen = Kitchen.objects.get(id=request.POST['kitchen_id'])
+    dateFormatted = datetime.datetime.today().strftime("%Y%m%d")
+    count =  kitchen.daily_order_count + 1
+    orderNum = f"{kitchen.designation_id}-{dateFormatted}-{count}"
+    notes = request.POST['notes']
+    status = "In Progress"
+
+
+    Order.objects.create(
+        order_num=orderNum, 
+        kitchen=kitchen,
+        notes = notes,
+        status = status
+        )
+
+    kitchen.daily_order_count += 1
+    kitchen.save()
+
+    return redirect('/')
+
+def kitchenDashboard(request, kitchen_id):
+    context = {
+        'kitchen': Kitchen.objects.get(id=kitchen_id)
+    }
+    return render(request, 'kitchenDashboard.html', context)
+
+def update_order_status(request, order_id):
+    const statuses = ["in progress", "ready", "done"]
+    
+    order = Order.objects.get(id=order_id)
+
+    return redirect('/')
